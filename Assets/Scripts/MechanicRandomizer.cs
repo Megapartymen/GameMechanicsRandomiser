1fe388792f2a42887a1b0f3abdd7a02154ec9be4
Megapartymen <pervi@list.ru>
2021-04-18 16:17:07 +0500

    Create a mechanic randomiser
    Написал скрипт рандомизирующий и выводящий игровые механики

---
+++ Assets/Scripts/MechanicRandomizer.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MechanicRandomizer : MonoBehaviour
+{
+    [SerializeField] private List<GameMechanic> _dimensionalMechanics;
+    [SerializeField] private List<GameMechanic> _triggerMechanics;
+    [SerializeField] private List<GameMechanic> _mentalMechanics;
+    [SerializeField] private List<GameMechanic> _combinedMechanics;    
+
+    [SerializeField] private Image _firstMechanicIcon;
+    [SerializeField] private TextMeshProUGUI _firstMechanicName;
+    [SerializeField] private Image _secondMechanicIcon;
+    [SerializeField] private TextMeshProUGUI _secondMechanicName;
+    [SerializeField] private Image _thirdMechanicIcon;
+    [SerializeField] private TextMeshProUGUI _thirdMechanicName;
+
+    //private List<GameMechanic> _mechanicsForRandom;
+    private GameMechanic _firstResultMechanic;
+    private GameMechanic _secondResultMechanic;
+    private GameMechanic _thirdResultMechanic;
+
+    private bool _useCombinedMechanics = false;
+
+    public void AddCombinedMechanicsFromList()
+    {
+        _useCombinedMechanics = true;
+    }
+
+    public void NoAddCombinedMechanicsFromList()
+    {
+        _useCombinedMechanics = false;
+    }
+
+
+    public void RandomizeAllMechanic(bool useCombinedMechanics)
+    {
+        List<GameMechanic> mechanicsForRandom = new List<GameMechanic>();
+
+        AddMechanicsToGlobalList(mechanicsForRandom, _dimensionalMechanics);
+        AddMechanicsToGlobalList(mechanicsForRandom, _triggerMechanics);
+        AddMechanicsToGlobalList(mechanicsForRandom, _mentalMechanics);
+
+        if (useCombinedMechanics)
+        {
+            AddMechanicsToGlobalList(mechanicsForRandom, _combinedMechanics);
+        }
+        
+        _firstResultMechanic = RandomizeMechanic(mechanicsForRandom);
+        ShowRandomResult(_firstResultMechanic, _firstMechanicIcon, _firstMechanicName);
+        _secondResultMechanic = RandomizeMechanic(mechanicsForRandom);
+        ShowRandomResult(_secondResultMechanic, _secondMechanicIcon, _secondMechanicName);
+        _thirdResultMechanic = RandomizeMechanic(mechanicsForRandom);
+        ShowRandomResult(_thirdResultMechanic, _thirdMechanicIcon, _thirdMechanicName);
+    }
+
+    private GameMechanic RandomizeMechanic(List<GameMechanic> list)
+    {
+        GameMechanic mechanic = list[Random.Range(0, list.Count)];
+        list.Remove(mechanic);
+
+        return mechanic;
+    }
+
+    private void ShowRandomResult(GameMechanic mechanic, Image icon, TextMeshProUGUI name)
+    {
+        icon.sprite = mechanic.Icon;
+        name.text = mechanic.Name;
+        icon.color = mechanic.Color;
+        name.color = mechanic.Color;
+    }
+
+    private void AddMechanicsToGlobalList(List<GameMechanic> globalList, List<GameMechanic> sourceList)
+    {
+        foreach (var mechanic in sourceList)
+        {
+            globalList.Add(mechanic);
+        }
+    }
+
+
+
+
+    //public void RandomizeMechanic()
+    //{
+    //    //заполнить поля с исключениями
+
+    //    if (_firstMechanicForExclude != null || _secondMechanicForExclude != null)
+    //    {
+    //        _mechanicsForRandom.Remove(_firstMechanicForExclude);
+    //        _mechanicsForRandom.Remove(_secondMechanicForExclude);
+    //    }
+
+    //    _resultMechanic = _mechanicsForRandom[Random.Range(0, _mechanicsForRandom.Count)];
+
+    //    _icon.sprite = _resultMechanic.Icon;
+    //    _name.text = _resultMechanic.Name;
+    //    _icon.color = _resultMechanic.Color;
+    //    _name.color = _resultMechanic.Color;
+    //}
+}
---
+++ Assets/Scripts/ScriptableObjects/GameMechanic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Mechanic", menuName = "Create New Mechanic", order = 51)]
+public class GameMechanic : ScriptableObject
+{
+    [SerializeField] private string _name;
+    [SerializeField] private Sprite _icon;  
+    [Tooltip("Dimensional — (242, 110, 222)\nTrigger — (98, 141, 250)\nMental — (92, 221, 231)\nCombined — (233, 147, 87)")]
+    [SerializeField] private Color _color;
+
+    public string Name => _name;
+    public Sprite Icon => _icon;
+    public Color Color => _color;
+}
+
